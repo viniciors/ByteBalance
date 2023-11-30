86fbc705056db4ca8623ac3428b1361b66c7efbd
Enzo Antunes Oliveira <enzo2004antunes@gmail.com>
2023-11-30 08:58:17 -0300

    Adicionado função cadastro, exibeMenu e copoDeAgua.

--- project.py
+++ project.py
@@ -6,7 +6,68 @@ def apresentacao():
             Tudo isso integrado tem o objetivo e ajudar a trazer uma rotina mais saudável a quem o utiliza.
           """)
 
+def cadastro():                     #Realiza um cadastrado para que o usuário possa acessar o programa
+    dadosUsuario = []
+    print("""
+            Para começar a utilizar o sistema, faça um simples cadastro com algumas poucas informações.
+          """)
+    
+    nome = input("Digite seu nome completo: ")
+    while not nome.replace(" ", "").isalpha():      #Verifica se o nome é composto apenas pro letras
+        print("ERRO. O nome deve conter apenas letras.")
+        nome=input("Digite seu nome completo: ")
+ 
+    email = input("Digite o seu email: ")
+    while "@" not in email or "." not in email or " " in email:         #Verifica se o email contém '@', '.' e não contém 'espaço'
+        print("ERRO. O email deve ser válido.")
+        email = input("Digite um email válido: ")
+ 
+    while True:                             #Trata entradas que não sejam números inteiros
+        try:
+            idade = int(input("Digite sua idade: "))
+            break
+        except ValueError:
+            print("ERRO. A idade deve ser preenchida com números inteiros.")
+ 
+    global peso
+    while True:                             #Trata entradas que não sejam números
+        try:
+            peso = float(input("Digite o seu peso em Kg: "))
+            break
+        except ValueError:
+            print("ERRO. O peso deve ser preenchido com números.")
+ 
+    global altura
+    while True:                             #Trata entradas que não sejam números                       
+        try:
+            altura = float(input("Digite sua altura em M: "))
+            break
+        except ValueError:
+            print("Erro. A altura deve ser preenchida com números e em metros.")
+ 
+    usuario = {
+            "Nome": nome,
+            "Email": email,
+            "idade": idade,
+            "Peso": peso,
+            "Altura": altura
+    }
+    dadosUsuario.append(usuario)
+    print("Cadastro efetuado com sucesso.")
+    print(f"Dados Cadastrados: {dadosUsuario}")
 
+def exibeMenu():
+    print("""
+                 ----------------------------------
+                |                                  |
+                |  1. Descubra o seu IMC           |
+                |  2. Quanta água deve consumir    |
+                |  3. Contador de Calorias         |
+                |  0. Encerrar o aplicativo        |
+                |                                  |
+                 ----------------------------------
+          """)
+    
 def imc(peso, altura):
     imc = peso / altura ** 2
     return imc
@@ -30,7 +91,7 @@ def classificacaoImc(imc):
     elif imc > 40:
         return "Obesidade extrema"
 
-def copoDeAgua(peso):
+def copoDeAgua(peso):               #Calcula a quantidade de água a ser ingerida de acordo com as informações fornecidas no cadastro
    quantidade = peso * 0.035
    return quantidade
 
